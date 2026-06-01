c9e3ce37968b136215a1b6975877652e63315732
zorearge-cloud <zorear.ge@gmail.com>
2026-06-01 08:30:41 +0000

    ttyr

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,8 @@ LINKS = [
     "https://docs.google.com/spreadsheets/d/1hVk6VgMFXWAukoQwMDIoOLrG8SD4UDLFFRH9VmDhXSE/export?format=xlsx",
     "https://docs.google.com/spreadsheets/d/1S1kTptWUEf705cBLw9P9mL6rrqbVjbcp1xk_hgQ-Ny0/export?format=xlsx",
     "https://docs.google.com/spreadsheets/d/1VKb6za4Fse5XrGawPG6qvrQZuFhDRGaAysmADGIC7Wc/export?format=xlsx",
-    "https://docs.google.com/spreadsheets/d/1F71jiUwqvxddv7jwJibisWVaFxQ3oLQPP3CohzK_idk/export?format=xlsx"
-    "https://docs.google.com/spreadsheets/d/1m3T2U7jsBe56fdTjPTI9l8Ix5kRXPd-34pNcmHnvdVU/export?format=xlsx"
+    "https://docs.google.com/spreadsheets/d/1F71jiUwqvxddv7jwJibisWVaFxQ3oLQPP3CohzK_idk/export?format=xlsx",
+    "https://docs.google.com/spreadsheets/d/1m3T2U7jsBe56fdTjPTI9l8Ix5kRXPd-34pNcmHnvdVU/export?format=xlsx",
 ]
 
 TARGET_TABS = ["has_air", "has_sea", "meh_air", "meh_sea", "ist_air", "ist_sea"]
